7cd03bbc572e494dd6d4158cca43df49c2ae7558
amansubudhi <amansubudhi12@gmail.com>
2022-03-18 16:59:57 +0530

    refactored

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const apiRoutes = require("./routes/api");
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = "mongodb://localhost:27017/blog-post";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/blog-post";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
