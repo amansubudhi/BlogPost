4bd349e40f5cf3c9f98538519d52947ecee1df53
amansubudhi <amansubudhi12@gmail.com>
2022-03-18 15:00:31 +0530

    deploying

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
+if(process.env.NODE_ENV !== "production") {
+   require('dotenv').config()
+}
+
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const { isLoggedIn, isAuthor, isCommentAuthor } = require('./middleware');
-const Blog = require('./models/blog');
-const Comment = require('./models/comment');
 const User = require('./models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const blogRoutes = require('./routes/blogs');
 const commentRoutes = require('./routes/comments');
 const userRoutes = require('./routes/users');
 
+const MongoDBStore = require("connect-mongo") (session);
 
-mongoose.connect('mongodb://localhost:27017/blog-post', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/blog-post';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -39,17 +44,36 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+     replaceWith: '_'
+}))
+
+const secret = process.env.SECRET || 'thisshouldbeasecret!';
+
+const store = new MongoDBStore ({
+   url: dbUrl,
+   secret,
+   touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e) {
+   console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
-   secret: 'thisshouldbeabettersecret!',
+   store,
+   name: 'session',
+   secret,
    resave: false,
    saveUninitialized: true,
    cookie: {
       httpOnly: true,
+      //secure: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7
    }
 }
+
 app.use(session(sessionConfig))
 app.use(flash());
 
@@ -92,7 +116,8 @@ app.all('*', (req, res, next) => {
   res.status(statusCode).render('error', { err })
  })
 
+ const port = process.env.PORT || 4000;
 
 app.listen(4000, () => {
-   console.log('Serving on port 4000')
+   console.log(`Serving on port ${port}`)
 })
